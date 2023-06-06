779a9a88c46c1391eba30741efdff7e61bdd6ea3
Sayd Karam <onltrabalho@gmail.com>
2023-06-05 22:59:32 -0300

    Mudanças necessárias

--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -1,8 +1,42 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
-  margin-bottom: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  background-color: #131313;
+  color: white;
 `
+export const ContainerInput = styled.div`
+  width: 50vw;
+  height: 50vh;
 
-export const ContainerLogin = styled.div``
-export const ContainerImgLogin = styled.div``
+  h1 {
+    font-size: 30px;
+  }
+
+  .User {
+    left: 10.58%;
+    right: 88.1%;
+    top: 48.17%;
+    bottom: 49.29%;
+  }
+
+  .Key {
+    color: #ffffff;
+    left: 10.45%;
+    right: 88.03%;
+    top: 56.52%;
+    bottom: 41.24%;
+  }
+`
+export const ContainerImg = styled.div`
+  width: 50vw;
+  height: 50vh;
+  img {
+    width: 710px;
+    height: 719.77px;
+    left: 780px;
+    top: 131px;
+  }
+`
