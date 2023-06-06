28b1edb62b74ebf2b7a6168774af7d8a645f6857
Sayd Karam <onltrabalho@gmail.com>
2023-06-05 22:58:55 -0300

    Rodas de todas as pags

--- src/navigation/routes.tsx
+++ src/navigation/routes.tsx
@@ -1,3 +1,5 @@
+import LayoutAnnotation from "../layouts/LayoutAnnotation"
+import LayoutHome from "../layouts/LayoutHome"
 import LayoutLogin from "../layouts/LayoutLogin"
 import { lazy } from "react"
 
@@ -16,10 +18,20 @@ function VerifyLayout(Component: React.FC) {
 }
 
 const Login = lazy(() => import("../pages/Login"))
+const Annotation = lazy(() => import("../pages/Annotation"))
+const Home = lazy(() => import("../pages/Home"))
 
 export const routes = [
   {
     element: <LayoutLogin />,
     children: [{ path: "/login", element: Private(Login) }],
+  },
+  {
+    element: <LayoutHome />,
+    children: [{ path: "/inicio", element: Private(Home) }],
+  },
+  {
+    element: <LayoutAnnotation />,
+    children: [{ path: "/Anotacao", element: Private(Annotation) }],
   },
 ]
