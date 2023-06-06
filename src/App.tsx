c2009a116b55684aacceae96acd1f14f5e78166e
Sayd Karam <onltrabalho@gmail.com>
2023-06-05 22:57:20 -0300

    Colocando estilo global no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import NavigationRoutes from "./navigation"
 import { BrowserRouter } from "react-router-dom"
+import { GlobalStyles } from "./styles/Globals"
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <GlobalStyles />
         <NavigationRoutes />
       </BrowserRouter>
     </div>
