f5d043f079df9cb8ff1b270198019753550ab34c
Sayd Karam <onltrabalho@gmail.com>
2023-06-24 13:36:15 -0300

    Rota da pág de salvar alterações

---
+++ src/components/Salve.tsx
@@ -0,0 +1,7 @@
+export default function Salve() {
+  return (
+    <div>
+      <h1>Página de Salvamento</h1>
+    </div>
+  )
+}
--- src/navigation/routes.tsx
+++ src/navigation/routes.tsx
@@ -1,6 +1,7 @@
 import LayoutAnnotation from "../layouts/LayoutAnnotation"
 import LayoutHome from "../layouts/LayoutHome"
 import LayoutLogin from "../layouts/LayoutLogin"
+import LayoutSalve from "../layouts/LayoutSalve"
 import { lazy } from "react"
 
 function Private(Component: React.FC) {
@@ -20,6 +21,7 @@ function VerifyLayout(Component: React.FC) {
 const Login = lazy(() => import("../pages/Login"))
 const Annotation = lazy(() => import("../pages/Annotation"))
 const Home = lazy(() => import("../pages/Home"))
+const Salve = lazy(() => import("../components/Salve"))
 
 export const routes = [
   {
@@ -37,5 +39,9 @@ export const routes = [
   {
     element: <LayoutAnnotation />,
     children: [{ path: "/Anotacao", element: Private(Annotation) }],
+  },
+  {
+    element: <LayoutSalve />,
+    children: [{ path: "/Salvo  ", element: Private(Salve) }],
   },
 ]
