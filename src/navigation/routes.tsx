8f8a16f383e0758ad21ea14a5314f6f67c541fa5
arthurssn <arthurnovaes55@gmail.com>
2023-07-09 19:31:02 -0300

    refactor: remove funcoes nao utilizadas

--- src/navigation/routes.tsx
+++ src/navigation/routes.tsx
@@ -4,44 +4,30 @@ import LayoutLogin from "../layouts/LayoutLogin";
 import LayoutSalve from "../layouts/LayoutSalve";
 import { lazy } from "react";
 
-function Private(Component: React.FC) {
-  const verify = false;
-
-  return <Component />;
-
-  // return verify ? <Component /> : <Login />;
-}
-
-function VerifyLayout(Component: React.FC) {
-  const verify = true;
-
-  return verify ? <Component /> : <LayoutLogin />;
-}
-
 const Login = lazy(() => import("../pages/Login"));
 const Annotation = lazy(() => import("../pages/Annotation"));
 const Home = lazy(() => import("../pages/Home"));
 const Salve = lazy(() => import("../components/Salve"));
 
+function Private(Component: React.FC) {
+  return <Component />;
+}
+
 export const routes = [
   {
     element: <LayoutLogin />,
-    children: [{ path: "/", element: Private(Login) }],
-  },
-  {
-    element: <LayoutLogin />,
-    children: [{ path: "/Login", element: Private(Login) }],
+    children: [{ path: "/login", element: Private(Login) }],
   },
   {
     element: <LayoutHome />,
-    children: [{ path: "/Inicio", element: Private(Home) }],
+    children: [{ path: "/", element: Private(Home) }],
   },
   {
     element: <LayoutAnnotation />,
-    children: [{ path: "/Anotacao", element: Private(Annotation) }],
+    children: [{ path: "/anotacao", element: Private(Annotation) }],
   },
   {
     element: <LayoutSalve />,
-    children: [{ path: "/Salvo", element: Private(Salve) }],
+    children: [{ path: "/salvo", element: Private(Salve) }],
   },
 ];
