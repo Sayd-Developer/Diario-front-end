49c64de6a514aa2d1f2a37b3355ef89e05bfa15f
Sayd Karam <onltrabalho@gmail.com>
2023-07-19 16:52:15 -0300

    feat(Rotas): Adicionei rotas a várias páginas

--- src/navigation/routes.tsx
+++ src/navigation/routes.tsx
@@ -4,6 +4,7 @@ import { lazy } from "react"
 const Login = lazy(() => import("../pages/Login"))
 const Annotation = lazy(() => import("../pages/Annotation"))
 const Home = lazy(() => import("../pages/Home"))
+const AnnotationRegister = lazy(() => import("../pages/AnnotationRegister"))
 
 function Private(Component: React.FC) {
   return <Component />
@@ -13,7 +14,10 @@ export const routes = [
   { children: [{ path: "/login", element: Private(Login) }] },
   {
     element: <LayoutApp />,
-    children: [{ path: "/", element: Private(Home) }],
+    children: [
+      { path: "/", element: Private(Home) },
+      { path: "/registro", element: Private(AnnotationRegister) },
+    ],
   },
   { path: "/anotacao", element: Private(Annotation) },
 ]
