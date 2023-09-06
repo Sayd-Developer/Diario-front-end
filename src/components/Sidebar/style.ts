e8a64cebc30390620c9decbf6c3af1dbd35b19c5
Sayd Karam <onltrabalho@gmail.com>
2023-09-06 19:27:19 -0300

    refactor: :art: Add ThemeProvider

--- src/components/Sidebar/style.ts
+++ src/components/Sidebar/style.ts
@@ -3,7 +3,7 @@ import styled from "styled-components"
 export const Container = styled.div`
   height: 100vh;
   width: 345px;
-  background-color: #131313;
+  background-color: ${(props) => props.theme.primary2};
   display: flex;
 `
 export const ContainerContent = styled.div`
